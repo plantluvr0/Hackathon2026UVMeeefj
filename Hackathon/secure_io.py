3964f800330f51bbb8c84853a2b1c2bd8820c5e4
ethan <ejorr@uvm.edu>
2026-02-28 10:30:06 -0500

    setting up secure

---
+++ Hackathon/secure_io.py
@@ -0,0 +1,13 @@
+
+def read_in(filename : str ) -> list:
+
+    masked_data = []
+    with open(filename, 'r') as f:
+        for line in f:
+            masked_data.append(line)
+    return masked_data
+
+
+def construct_pile(vec_strings) -> str:
+
+def mask_pile
