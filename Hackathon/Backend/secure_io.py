0da61894f1a3a4588b1157d902303db714b820ae
ethan <ejorr@uvm.edu>
2026-02-28 13:24:44 -0500

    "done encryption"

--- Hackathon/Backend/secure_io.py
+++ Hackathon/Backend/secure_io.py
@@ -1,47 +1,53 @@
 from dataclasses import dataclass
 import csv
-def read_in(filename : str ) -> list:
-    patient_list = []
+import secrets
 
+
+def read_in(filename : str ) -> tuple:
+    patient_list = []
     with open(filename, 'r') as csv_read:
         csvfile_reader = csv.reader(csv_read)
 
         reader = csv.reader(csv_read, delimiter=',')
-        if next(csvfile_reader, None) is None:
-            next(reader)
+        header = next(reader)
         for row in reader:
             patient_list.append(row)
-        return patient_list
-def mask(info :str, index: int) -> str:
+        return patient_list, header
+def mask(info :str) -> str:
 
-    masked_info = str(index) +
+    mask_num =  int(hex(secrets.randbits(len(info))), 0)
+    hex_info = int(info.encode().hex(), 0)
+    masked_info = mask_num ^ hex_info
+    return str(masked_info)
 
-    return masked_info
-
-def replace_proprietery(patient_data :list) -> list:
+def replace_proprietary(patient_data :list) -> list:
     curr_data = ""
-    def store_proprietery(enc_val, real_val):
-        pass
-    for i in range(7):
+    def store_proprietary(enc_val, real_val):
+        with open("../key_val.csv", 'a') as key_file:
+            writer = csv.writer(key_file)
+            writer.writerow([enc_val, real_val])
+    for i in range(1, 7):
         curr_data = patient_data[i]
-        sec_data = mask(patient_data[i], i)
+        sec_data = mask(patient_data[i])
         patient_data[i] = sec_data
-        store_proprietery(sec_data, curr_data)
+        store_proprietary(sec_data, curr_data)
 
+    return patient_data
+
+
+def decrypt(fmt_data :dict) -> list:
     return []
-def prep_inejct(masked_data: list):
-    return 0
-
-def decrypt():
-    pass
 
 
-def encrypt(filename :str):
-    patient_set = read_in(filename)
+def encrypt(filename :str) -> tuple:
+    patient_set, header = read_in(filename)
     for patient in patient_set:
-        replace_proprietery(patient)
+        replace_proprietary(patient)
+    return patient_set, header
 
-
+def inject_data(filename) -> tuple:
+    return encrypt(filename)
+    pass
 if __name__ == "__main__":
 
     encrypt("..\patient_info.csv")
