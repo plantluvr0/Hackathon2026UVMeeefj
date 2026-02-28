371650bc420af0580ba0cee7b33f93858234e80d
ethan <ejorr@uvm.edu>
2026-02-28 12:05:02 -0500

    more secure

--- Hackathon/Backend/secure_io.py
+++ Hackathon/Backend/secure_io.py
@@ -1,13 +1,34 @@
-
+from dataclasses import dataclass
+import csv
 def read_in(filename : str ) -> list:
+    patient_list = []
 
-    masked_data = []
-    with open(filename, 'r') as f:
-        for line in f:
-            masked_data.append(line)
-    return masked_data
+    with open(filename, 'r') as csv_read:
+        csvfile_reader = csv.reader(csv_read)
+
+        reader = csv.reader(csv_read, delimiter=',')
+        if next(csvfile_reader, None) is None:
+            next(reader)
+        for row in reader:
+            patient_list.append(row)
+        return patient_list
+
+def replace_proprietery() -> list:
+    def store_proprietery(name_mask_pair: dict):
+        return 0
+    return []
+def prep_inejct(masked_data: list):
+    return 0
+
+def decrypt():
+    pass
 
 
-def construct_pile(vec_strings) -> str:
+def encrypt(filename :str):
+    patient_set = read_in(filename)
+    for patient in patient_set:
+        p_key = patient[0]+patient[1]+patient[2]+patient[3]+patient[4]+patient[5]+patient[6]+patient[7]
+        print(p_key)
 
-def mask_pile
+if __name__ == "__main__":
+    encrypt("..\patient_info.csv")
